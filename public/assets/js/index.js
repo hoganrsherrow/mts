243d778f52e3080f26ed06ec267fa7fb5a651e1d
hogan <hogan.r.sherrow@vanderbilt.edu>
2023-03-05 00:23:22 -0600

    began working on timer

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,12 +1,17 @@
-const btn = document.getElementsByName("A").length === 1 ? document.getElementsByName("A")[0] : document.getElementsByName("A");
+// const btn = document.getElementsByName("A").length === 1 ? document.getElementsByName("A")[0] : document.getElementsByName("A");
+
+const startBtn = document.getElementById("start");
+
+
+/***
+ * 
+ ***/
 
 // Get the input fields and buttons
-const hoursInput = document.getElementById("hours");
-const minutesInput = document.getElementById("minutes");
-const secondsInput = document.getElementById("seconds");
-const startButton = document.getElementById("start-button");
-const stopButton = document.getElementById("stop-button");
-const resetButton = document.getElementById("reset-button");
+const timerInput = document.getElementById("timer-input");
+const hoursInput = document.getElementById("hours-input");
+const minutesInput = document.getElementById("minutes-input");
+const secondsInput = document.getElementById("seconds-input");
 
 // Get the countdown display element
 const countdownDisplay = document.getElementById("timer-countdown");
@@ -14,86 +19,60 @@ const countdownDisplay = document.getElementById("timer-countdown");
 // Initialize the countdown timer
 let countdown;
 
-// Start the countdown timer
-function startCountdown() {
-  // Disable the input fields and start button
-  hoursInput.disabled = true;
-  minutesInput.disabled = true;
-  secondsInput.disabled = true;
-  startButton.disabled = true;
+// Get total seconds for countdown timer
+function getCountdown(hoursInput, minutesInput, secondsInput) {
+  console.log(hoursInput.value, minutesInput.value, secondsInput.value);
+  
+  // Total number of seconds
+  countdown = Number(hoursInput.value) * 3600 + Number(minutesInput.value) * 60 + Number(secondsInput.value);
+  createTimerCountdownEl(countdown);
 
-  // Get the user input values
-  const hours = parseInt(hoursInput.value);
-  const minutes = parseInt(minutesInput.value);
-  const seconds = parseInt(secondsInput.value);
 
-  // Calculate the total number of seconds
-  const totalSeconds = hours * 3600 + minutes * 60 + seconds;
-
-  // Set the countdown timer interval
-  countdown = setInterval(() => {
-    // Calculate the remaining time
-    const remainingSeconds = totalSeconds - 1;
-
-    // Calculate the hours, minutes, and seconds from the remaining time
-const hours = Math.floor(remainingSeconds / 3600);
-const minutes = Math.floor((remainingSeconds % 3600) / 60);
-const seconds = remainingSeconds % 60;
-
-// Format the remaining time
-const formattedTime = `${String(hours).padStart(2, "0")}:${String(
-  minutes
-).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
-
-// Update the countdown display
-countdownDisplay.textContent = formattedTime;
-
-// Check if the countdown is finished
-if (remainingSeconds === 0) {
-  clearInterval(countdown);
-  countdownDisplay.textContent = "00:00:00";
-  hoursInput.disabled = false;
-  minutesInput.disabled = false;
-  secondsInput.disabled = false;
-  startButton.disabled = false;
-  alert("Countdown finished!");
 }
 
-// Decrement the total number of seconds
-totalSeconds--;
-}, 1000);
+const createTimerCountdownEl = (countdown) => {
+  console.log(countdown);
+  countdownDisplay.classList.remove('hide');
+  // Hours
+  let hourDisplay = document.createElement('div');
+  hourDisplay.setAttribute('name', 'hours');
+  hourDisplay.innerText = (Math.floor(countdown / 3600) > 9 ? Math.floor(countdown / 3600) : `0${Math.floor(countdown / 3600)}`);
+
+  // Minutes
+  let minDisplay = document.createElement('div');
+  minDisplay.setAttribute('name', 'minutes');
+  minDisplay.innerText = (Math.floor((countdown % 3600) / 60) > 9 ? Math.floor((countdown % 3600) / 60) : `0${Math.floor((countdown % 3600) / 60)}`);
+
+  // Seconds
+  let secDisplay = document.createElement('div');
+  secDisplay.setAttribute('name', 'seconds');
+  secDisplay.innerText = (countdown > 9 ? countdown : `0${countdown}`);
+
+  timerInput.classList.add('hide');
+  countdownDisplay.append(hourDisplay, minDisplay, secDisplay);
+
+  setInterval(updateTimer(countdown, hourDisplay, minDisplay, secDisplay), 1000);
 }
 
-// Stop the countdown timer
-function stopCountdown() {
-clearInterval(countdown);
-hoursInput.disabled = false;
-minutesInput.disabled = false;
-secondsInput.disabled = false;
-startButton.disabled = false;
-}
-
-// Reset the countdown timer
-function resetCountdown() {
-clearInterval(countdown);
-countdownDisplay.textContent = "00:00:00";
-hoursInput.value = "";
-minutesInput.value = "";
-secondsInput.value = "";
-hoursInput.disabled = false;
-minutesInput.disabled = false;
-secondsInput.disabled = false;
-startButton.disabled = false;
+// create updateTimer function
+function updateTimer(countdown, hourDisplay, minDisplay, secDisplay) {
+  if(countdown > 0) {
+    countdown--;
+    hourDisplay.innerText = (Math.floor(countdown / 3600) > 9 ? Math.floor(countdown / 3600) : `0${Math.floor(countdown / 3600)}`);
+    minDisplay.innerText = (Math.floor(countdown / 3600 * 60) > 9 ? Math.floor(countdown / 60) : `0${Math.floor(countdown / 60)}`);
+    secDisplay.innerText = (countdown > 9 ? countdown : `0${countdown}`);
+  }
 }
 
 // Add event listeners to the buttons
-startButton.addEventListener("click", startCountdown);
-stopButton.addEventListener("click", stopCountdown);
-resetButton.addEventListener("click", resetCountdown);
+startBtn.addEventListener("click", () => {
+  getCountdown(hoursInput, minutesInput, secondsInput);
+});
+
 
 document.onreadystatechange = () => {
     if(document.readyState === `complete`) {
         console.log(`The document is ready`);
-        console.log(btn);
+        // console.log(btn);
     }
 };
